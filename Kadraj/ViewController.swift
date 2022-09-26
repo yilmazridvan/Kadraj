0c2846da9ea5f9218cbfbcc794c1cbfda3b98f58
Rıdvan Yılmaz <ridvanyilmaz5098@gmail.com>
2022-09-26 19:10:18 +0300

    Initial Commit

---
+++ Kadraj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadraj
+//
+//  Created by Rıdvan Yılmaz on 26.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
