d201f6f767ed823ed66769d2248156215f3bf6dc
Rıdvan Yılmaz <ridvanyilmaz5098@gmail.com>
2022-09-27 17:55:33 +0300

    It's the first commit of my project, it will be developed.

---
+++ Kadraj/Service/Webservice.swift
@@ -0,0 +1,16 @@
+//
+//  Webservice.swift
+//  Kadraj
+//
+//  Created by Rıdvan Yılmaz on 26.09.2022.
+//
+
+import Foundation
+
+
+class Webservice {
+    
+    func takedata(url: URL) {
+        
+    }
+}
